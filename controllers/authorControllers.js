3118867e88253acb9a787392cb1be4655eac1fce
ShyPenguin <116872355+ShyPenguin@users.noreply.github.com>
2022-12-28 17:31:32 +0800

    Finish Author Routes

--- controllers/authorControllers.js
+++ controllers/authorControllers.js
@@ -1,4 +1,5 @@
 const Author = require("../models/author")
+const Book = require("../models/book")
 
 const author_index = async(req, res) => {
   let searchOptions = {}
@@ -27,8 +28,7 @@ const author_new_post = async(req, res) => {
   })
   try {
     const newAuthor = await author.save();
-    // res.redirect(`authors/${newAuthor.id}`)
-    res.redirect(`authors`)
+    res.redirect(`authors/${newAuthor.id}`)
   } catch {
     res.render('authors/new', {
       author: author,
@@ -37,8 +37,69 @@ const author_new_post = async(req, res) => {
   }
 }
 
+const author_details_get = async (req, res) => {
+  try{
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({ author: author.id }).limit(6).exec()
+    res.render('authors/details', {
+      author: author,
+      booksByAuthor: books
+    })
+  } catch {
+    res.redirect('/')
+  }
+}
+
+const author_edit_get = async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render("authors/edit", { author: author});
+  } catch {
+    res.render("/authors")
+  }
+
+}
+
+const author_update_put = async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    author.name = req.body.name
+    await author.save()
+    res.redirect(`/authors/${author.id}`)
+  } catch {
+    if(author == null) {
+      res.redirect('/')
+    } else {
+      res.render('authors/edit', {
+        author: author,
+        errorMessage: 'Error updating Author'
+      })
+    }
+  }
+}
+
+const author_delete = async(req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    await author.remove()
+    res.redirect(`/authors`)
+  } catch {
+    if(author == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/authors/${author.id}`)
+    }
+  }
+}
+
 module.exports = {
   author_index,
   author_new_get,
-  author_new_post
+  author_new_post,
+  author_details_get,
+  author_edit_get,
+  author_update_put,
+  author_delete
 }
--- routes/authors.js
+++ routes/authors.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const authorControllers = require("../controllers/authorControllers")
 
 //Author Index
-router.get("/", authorControllers.author_index);
+router.get("/", authorControllers.author_index)
 
 //New Author
-router.get("/new", authorControllers.author_new_get);
+router.get("/new", authorControllers.author_new_get)
 
 //Create Author
 router.post("/", authorControllers.author_new_post)
 
+router.route("/:id")
+      .get(authorControllers.author_details_get)
+      .put(authorControllers.author_update_put)
+      .delete(authorControllers.author_delete)
 
+router.get("/:id/edit", authorControllers.author_edit_get)
 module.exports = router
